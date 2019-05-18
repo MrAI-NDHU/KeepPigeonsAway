b3d2aa075c9020ff55c49635a585efda68e2d3af
Yu-Jin Lin <abc1236762@outlook.com>
2019-05-18 18:22:48 +0800

    bug fix

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ from threading import Thread
 from typing import Any
 import logging
 import pickle
+import random
 import sys
 import time
+import termios
 
 from Jetson import GPIO
 import cv2
@@ -23,11 +25,10 @@ class DriveAwayPigeons:
         self.areas_cnt = self.split_w * self.split_h
         self.angle_prec = angle_prec
         self.laser_pin = 18
+        self.sweep_around_times = 3
         self.can_detect, self.can_sweep = False, False
         self.cap = cv2.VideoCapture(0)
-        self.cap_ratio = \
-            self.cap.get(cv2.CAP_PROP_FRAME_WIDTH) \
-            / self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+        self.cap_ratio = 16 / 9
         self.split_line_color = (0x00, 0xFF, 0x00)
         self.areas_canter_color = (0x00, 0x00, 0xFF)
         
@@ -36,8 +37,7 @@ class DriveAwayPigeons:
         
         self.showing_w, self.showing_h = 0, 0
         self.areas_angle = [[{}]]
-        self.sweep_around_times = 0
-        self.sweep_angle_amp = {}
+        self.area_angle_spacing = {}
         
         self.arm = self.set_arm()
         self.init_darknet()
@@ -75,14 +75,14 @@ class DriveAwayPigeons:
         self.darknet_img = darknet.make_image(
             self.darknet_net_w, self.darknet_net_h, 3)
         self.showing_h = min(self.darknet_net_w, self.darknet_net_h)
-        self.showing_w = self.showing_h * self.cap_ratio
+        self.showing_w = int(round(self.showing_h * self.cap_ratio))
     
     def init_laser(self):
         GPIO.setmode(GPIO.BCM)
-        GPIO.setup(self.laser_pin, GPIO.OUT, initial=GPIO.LOW)
+        GPIO.setup(self.laser_pin, GPIO.OUT, initial=GPIO.HIGH)
         if TEST_MODE:
             logging.info("init_laser: test start")
-            for i in range(4):
+            for i in range(0):
                 time.sleep(2)
                 self.open_laser()
                 time.sleep(2)
@@ -96,7 +96,7 @@ class DriveAwayPigeons:
         GPIO.output(self.laser_pin, GPIO.LOW)
     
     def set_arm(self) -> ControllerForPCA9685:
-        mg995_sec_per_angle = (0.16 - 0.2) / (6.0 - 4.8) * (5.0 - 4.8) + 0.2
+        mg995_sec_per_angle = ((0.16 - 0.2) / (6.0 - 4.8) * (5.0 - 4.8) + 0.2) / 60.0
         mg995_tilt = Servo(0.0, 180.0, 150.0, 510.0, 50.0, mg995_sec_per_angle)
         mg995_pan = Servo(0.0, 180.0, 180.0, 630.0, 50.0, mg995_sec_per_angle)
         servos = {self.Y: mg995_tilt, self.X: mg995_pan}
@@ -104,21 +104,24 @@ class DriveAwayPigeons:
         return ControllerForPCA9685(servos, chs, 60.0)
     
     def check_areas_angle(self):
-        for y in range(len(self.areas_angle)):
-            for x in range(y):
+        for y in range(self.split_h):
+            for x in range(self.split_w):
                 if self.areas_angle[y][x] is None:
                     raise Exception("failed to check areas angle")
                 if TEST_MODE:
-                    n = y * len(self.areas_angle) + x
-                    logging.info("check_areas_angle: check {}".format(n))
+                    n = y * self.split_w + x
+                    logging.info("check_areas_angle: check {}: {}".format(n, self.areas_angle[y][x]))
                     self.arm.rotate(self.areas_angle[y][x], False)
                     self.open_laser()
                     time.sleep(2)
-                    self.sweep_area(x, y)
+                    self.can_sweep = True
+                    for i in range(100):
+                        self.sweep_area(x, y)
+                    self.can_sweep = False
                     self.close_laser()
     
     def init_areas_angle(self):
-        self.areas_angle = [[None] * self.split_w] * self.split_h
+        self.areas_angle = [[None] * self.split_w for i in range(self.split_h)]
         filepath = input("Load areas angle: ").strip()
         if filepath != "":
             with open(filepath, "rb") as f:
@@ -135,14 +138,15 @@ class DriveAwayPigeons:
                 elif keyboard.is_pressed("down"):
                     self.arm.rotate({self.Y: self.angle_prec}, True)
                 elif keyboard.is_pressed("left"):
-                    self.arm.rotate({self.X: -self.angle_prec}, True)
-                elif keyboard.is_pressed("right"):
                     self.arm.rotate({self.X: self.angle_prec}, True)
+                elif keyboard.is_pressed("right"):
+                    self.arm.rotate({self.X: -self.angle_prec}, True)
                 elif keyboard.is_pressed("s"):
+                    termios.tcflush(sys.stdin, termios.TCIOFLUSH)
                     i = int(input("Input area number in range [{},{}]: "
                                   .format(0, self.split_w * self.split_h - 1)))
-                    self.areas_angle[i // self.split_h][i % self.split_w] = \
-                        self.arm.current_angles
+                    self.areas_angle[i // self.split_w][i % self.split_w] = \
+                        self.arm.current_angles.copy()
             self.close_laser()
             self.init_sweep_attrs()
             self.check_areas_angle()
@@ -152,40 +156,31 @@ class DriveAwayPigeons:
                     pickle.dump(self.areas_angle, f)
     
     def init_sweep_attrs(self):
-        area_angle_spacing = {self.X: 0.0, self.Y: 0.0}
-        for i in self.areas_angle:
-            for j in range(1, len(i)):
-                area_angle_spacing[self.X] = max(i[j][self.X] - i[
-                    j - 1][self.X], area_angle_spacing[self.X])
-                area_angle_spacing[self.Y] = max(i[j][self.Y] - i[
-                    j - 1][self.Y], area_angle_spacing[self.Y])
-        t = int(round(min(area_angle_spacing[self.X],
-                          area_angle_spacing[self.Y]) / 2 / self.angle_prec))
-        self.sweep_angle_amp[self.X] = area_angle_spacing[self.X] / t
-        self.sweep_angle_amp[self.Y] = area_angle_spacing[self.Y] / t
-        self.sweep_around_times = t
+        self.area_angle_spacing = {self.X: 0.0, self.Y: 0.0}
+        for i in range(1, self.areas_cnt):
+            ax1, ay1 = i % self.split_w, i // self.split_w
+            ax0, ay0 = (i - 1) % self.split_w, (i - 1) // self.split_w
+            if ax1 > ax0:
+                area1_x = self.areas_angle[ay1][ax1][self.X]
+                area0_x = self.areas_angle[ay0][ax0][self.X]
+                self.area_angle_spacing[self.X] = \
+                    max(abs(area1_x - area0_x), self.area_angle_spacing[self.X])
+            if ay1 > ay0:
+                area1_y = self.areas_angle[ay1][ax1][self.Y]
+                area0_y = self.areas_angle[ay0][ax0][self.Y]
+                self.area_angle_spacing[self.Y] = \
+                    max(abs(area1_y - area0_y), self.area_angle_spacing[self.Y])
     
     def sweep_area(self, area_x: int, area_y: int):
+        if not self.can_sweep:
+            return
         area_angle = self.areas_angle[area_y][area_x]
-        self.arm.rotate(area_angle, False)
-        for i in range(self.sweep_around_times):
-            if not self.can_sweep:
-                return
-            area_angle[self.X] -= self.sweep_angle_amp[self.X] / 2
-            area_angle[self.Y] -= self.sweep_angle_amp[self.Y] / 2
-            self.arm.rotate(area_angle, False)
-            if not self.can_sweep:
-                return
-            self.arm.rotate({self.X: self.sweep_angle_amp[self.X] * i}, True)
-            if not self.can_sweep:
-                return
-            self.arm.rotate({self.Y: self.sweep_angle_amp[self.Y] * i}, True)
-            if not self.can_sweep:
-                return
-            self.arm.rotate({self.X: self.sweep_angle_amp[self.X] * -i}, True)
-            if not self.can_sweep:
-                return
-            self.arm.rotate({self.Y: self.sweep_angle_amp[self.Y] * -i}, True)
+        x = random.uniform(area_angle[self.X] - self.area_angle_spacing[self.X] / 2, \
+            area_angle[self.X] + self.area_angle_spacing[self.X] / 2)
+        y = random.uniform(area_angle[self.Y] - self.area_angle_spacing[self.Y] / 2, \
+            area_angle[self.Y] + self.area_angle_spacing[self.Y] / 2)
+        time.sleep(0.05)
+        self.arm.rotate({self.X: x, self.Y: y}, False)
     
     def get_cap_img(self) -> Any:
         _, img = self.cap.read()
@@ -205,7 +200,7 @@ class DriveAwayPigeons:
             for sy in range(1, self.split_h * 2 + 1, 2):
                 x = int(round(self.showing_w / (self.split_w * 2) * sx))
                 y = int(round(self.showing_h / (self.split_h * 2) * sy))
-                img = cv2.circle(img, (x, y), 2, self.areas_canter_color, 1)
+                img = cv2.circle(img, (x, y), 4, self.areas_canter_color, 1)
         return img
     
     def thd_detecting_func(self):
@@ -256,6 +251,9 @@ TEST_MODE = True
 
 
 def main():
+    logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+    random.seed()
+    
     split_w, split_h = 4, 3
     if len(sys.argv) == 2:
         split_w, split_h = int(sys.argv[1]), int(sys.argv[1])
@@ -263,7 +261,7 @@ def main():
         split_w, split_h = int(sys.argv[1]), int(sys.argv[2])
     d = None
     try:
-        d = DriveAwayPigeons(split_w, split_h, 1.0)
+        d = DriveAwayPigeons(split_w, split_h, 0.05)
     except KeyboardInterrupt:
         del d
 
